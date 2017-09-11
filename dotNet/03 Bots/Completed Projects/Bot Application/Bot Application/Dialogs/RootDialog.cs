75db56fe859d1e22c8c07aa1bd7a85db960371b5
shroffmanish <shroff.manish@gmail.com>
2017-09-11 22:25:47 +1000

    Refactoring - Created ConnectwiseHelper class

--- dotNet/03 Bots/Completed Projects/Bot Application/Bot Application/Dialogs/RootDialog.cs
+++ dotNet/03 Bots/Completed Projects/Bot Application/Bot Application/Dialogs/RootDialog.cs
@@ -13,6 +13,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json.Linq;
 using Bot_Application.Entities;
+using Bot_Application.Helper;
 
 namespace Bot_Application
 {
@@ -22,8 +23,6 @@ namespace Bot_Application
     [Serializable]
     public class RootDialog : IDialog<object>
     {
-        public const string cwURI = "https://api-aus.myconnectwise.net/v2017_5/apis/3.0/";
-
         public Task StartAsync(IDialogContext context)
         {
             context.Wait(MessageReceivedAsync);
@@ -75,12 +74,12 @@ namespace Bot_Application
                 // Parse the command and go do the right thing
                 if (cmd.Contains("status"))
                 {
-                    Ticket ticketDetails = GetTicketStatus(string.Join(" ", q));
+                    Ticket ticketDetails = ConnectWiseHelper.GetTicketStatus(string.Join(" ", q));
                     await SendStatusMessage(context, ticketDetails);
                 }
                 else if (cmd.Contains("contact"))
                 {
-                    Company companyDetails = GetContactDetails(string.Join(" ", q));
+                    Company companyDetails = ConnectWiseHelper.GetContactDetails(string.Join(" ", q));
                     await SendContactDetailsMessage(context, companyDetails);
                 }
                 //else if (cmd.Contains("link"))
@@ -267,105 +266,6 @@ namespace Bot_Application
                 + "* To create a deep link, you can type **link** followed by the tab name";
 
             await context.PostAsync(helpMessage);
-        }
-
-        private static HttpResponseMessage GetCWResponse(string uri)
-        {
-            HttpResponseMessage response = null;
-            string accessToken = "S2xvdWRUcmFpbmluZytxcHBWZkFNZlVWMXJaZ0tKOk1vU1RCdURzMG5MRlp5b3A=";
-            HttpClient httpClient = new HttpClient();
-            httpClient.BaseAddress = new Uri(uri);
-            httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + accessToken);
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            response = httpClient.GetAsync(uri).Result;
-
-            return response;
-
-        }
-
-
-        private Company GetContactDetails(string companyId)
-        {
-            Company company = null;
-            AccountManager accountManager = null;
-            JObject companyObject = null;
-
-            string urlCompany = string.Format("{0}company/companies/{1}", cwURI, companyId);
-
-            HttpResponseMessage responseCompany = GetCWResponse(urlCompany);
-
-            if (responseCompany.IsSuccessStatusCode)
-            {
-                using (HttpContent content = responseCompany.Content)
-                {
-                    Task<string> result = content.ReadAsStringAsync();
-                    companyObject = JObject.Parse(result.Result);
-                    company = new Company { Name = companyObject["name"].ToString(), Address = companyObject["addressLine1"].ToString() };
-                }
-
-                JObject defaultContactObject = JObject.Parse(companyObject["defaultContact"].ToString());
-                string accountManagerId = defaultContactObject["id"].ToString();
-                string accountManagerName = defaultContactObject["name"].ToString();
-
-                string contactInfoUri = string.Format("{0}company/contacts/{1}/communications", cwURI, accountManagerId);
-
-                HttpResponseMessage responsecontactInfo = GetCWResponse(contactInfoUri);
-
-                if (responsecontactInfo.IsSuccessStatusCode)
-                {
-                    using (HttpContent content1 = responsecontactInfo.Content)
-                    {
-                        Task<string> resultContactInfo = content1.ReadAsStringAsync();
-                        JArray contactInfoObject = JArray.Parse(resultContactInfo.Result);
-                        string phoneNumber = "";
-                        string email = "";
-                        foreach (var item in contactInfoObject.Children())
-                        {
-                            var itemProperties = item.Children<JProperty>();
-                            var myElement = itemProperties.FirstOrDefault(x => x.Name == "communicationType");
-                            if (myElement.Value.ToString() == "Phone")
-                            {
-                                phoneNumber = itemProperties.FirstOrDefault(x => x.Name == "value").Value.ToString();
-                            }
-                            if (myElement.Value.ToString() == "Email")
-                            {
-                                email = itemProperties.FirstOrDefault(x => x.Name == "value").Value.ToString();
-                            }
-                        }
-
-                        accountManager = new AccountManager { Name = accountManagerName, Email = email, PhoneNumber = phoneNumber };
-
-                        company.AccountManager = accountManager;
-                    }
-                }
-            }
-            return company;
-        }
-
-        private Ticket GetTicketStatus(string ticketNumber)
-        {
-            Ticket tickDetails = null;
-            string url = string.Format("{0}/service/tickets/{1}", cwURI, ticketNumber);
-
-            HttpResponseMessage response = GetCWResponse(url);
-
-            if (response.IsSuccessStatusCode)
-            {
-                using (HttpContent content = response.Content)
-                {
-                    Task<string> result = content.ReadAsStringAsync();
-                    JObject ticketObject = JObject.Parse(result.Result);
-
-                    tickDetails = new Ticket
-                    {
-                        Title = ticketObject["summary"].ToString(),
-                        SubTitle = ticketNumber,
-                        Text = ticketObject["status"]["name"].ToString()
-                    };
-                }
-            }
-
-            return tickDetails;
         }
     }
 }
