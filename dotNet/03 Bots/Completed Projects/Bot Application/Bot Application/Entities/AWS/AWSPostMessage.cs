338c931901b4cfad50049e73c022711db8640475
Michael Hewett <y2trooper@gmail.com>
2017-09-14 15:14:18 +1000

    - adding AWSHelper with models to create POST messages and requesting hardcoded VMs for now

---
+++ dotNet/03 Bots/Completed Projects/Bot Application/Bot Application/Entities/AWS/AWSPostMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot_Application.Entities.AWS
+{
+    [Serializable]
+    public class AWSPostMessage
+    {
+        public int CompanyId { get; set; }
+        public string ServiceDescription { get; set; }
+        public int ServiceId { get; set; }
+        public string ServiceName { get; set; }
+        public string TicketDescription { get; set; }
+        public TicketFields TicketFields { get; set; }
+        public int TicketId { get; set; }
+        public string TicketRequesterEmail { get; set; }
+        public string TicketRequesterName { get; set; }
+        public string TicketRequesterPhone { get; set; }
+        public string TicketSubject { get; set; }
+    }
+}
