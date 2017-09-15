e542e818c0d775e3c31cecf1f04896a3bbbef194
Asish Padhy <asish2k14@outlook.com>
2017-09-15 16:09:35 +1000

    node js compose extensions
    deployment is not complete - but debug works

---
+++ microsoft-teams-compose-extensions/src/Bot.ts
@@ -0,0 +1,103 @@
+import * as builder from "botbuilder";
+import * as config from "config";
+import { RootDialog } from "./dialogs/RootDialog";
+import { SetLocaleFromTeamsSetting } from "./middleware/SetLocaleFromTeamsSetting";
+import { StripBotAtMentions } from "./middleware/StripBotAtMentions";
+// import { SetAADObjectId } from "./middleware/SetAADObjectId";
+import { LoadBotChannelData } from "./middleware/LoadBotChannelData";
+import { Strings } from "./locale/locale";
+import { loadSessionAsync } from "./utils/DialogUtils";
+import * as teams from "botbuilder-teams";
+import { ComposeExtensionHandlers } from "./composeExtension/ComposeExtensionHandlers";
+
+// =========================================================
+// Bot Setup
+// =========================================================
+
+export class Bot extends builder.UniversalBot {
+
+    constructor(
+        private _connector: teams.TeamsChatConnector,
+        private botSettings: any,
+    ) {
+        super(_connector, botSettings);
+        this.set("persistConversationData", true);
+
+        // Root dialog
+        new RootDialog(this).createChildDialogs();
+
+        // Add middleware
+        this.use(
+            // currently this middleware cannot be used because there is an error using it
+            // with updating messages examples
+            // builder.Middleware.sendTyping(),
+
+            // set on "receive" of message
+            new SetLocaleFromTeamsSetting(),
+
+            // set on "botbuilder" (after session created)
+            new StripBotAtMentions(),
+            // new SetAADObjectId(),
+            new LoadBotChannelData(this.get("channelStorage")),
+        );
+
+        // setup invoke payload handler
+        this._connector.onInvoke(this.getInvokeHandler(this));
+
+        // setup conversation update handler for things such as a memberAdded event
+        this.on("conversationUpdate", this.getConversationUpdateHandler(this));
+
+        // setup compose extension handlers
+        // onQuery is for events that come through the compose extension itself including
+        // config and auth responses from popups that were started in the compose extension
+        // onQuerySettingsUrl is only used when the user selects "Settings" from the three dot option
+        // next to the compose extension's name on the list of compose extensions
+        // onSettingsUpdate is only used for the response from the popup created by the
+        // onQuerySettingsUrl event
+        this._connector.onQuery("search123", ComposeExtensionHandlers.getOnQueryHandler(this));
+        this._connector.onQuerySettingsUrl(ComposeExtensionHandlers.getOnQuerySettingsUrlHandler());
+        this._connector.onSettingsUpdate(ComposeExtensionHandlers.getOnSettingsUpdateHandler(this));
+    }
+
+    // Handle incoming invoke
+    private getInvokeHandler(bot: builder.UniversalBot): (event: builder.IEvent, callback: (err: Error, body: any, status?: number) => void) => void {
+        return async function (
+            event: builder.IEvent,
+            callback: (err: Error, body: any, status?: number) => void,
+        ): Promise<void>
+        {
+            let session = await loadSessionAsync(bot, event);
+            if (session) {
+                // Clear the stack on invoke, as many builtin dialogs don't play well with invoke
+                // Invoke messages should carry the necessary information to perform their action
+                session.clearDialogStack();
+
+                let payload = (event as any).value;
+
+                // Invokes don't participate in middleware
+                // If payload has an address, then it is from a button to update a message so we do not what to send typing
+                if (!payload.address) {
+                    session.sendTyping();
+                }
+
+                if (payload && payload.dialog) {
+                    session.beginDialog(payload.dialog, payload);
+                }
+            }
+            callback(null, "", 200);
+        };
+    }
+
+    // set incoming event to any because membersAdded is not a field in builder.IEvent
+    private getConversationUpdateHandler(bot: builder.UniversalBot): (event: any) => void {
+        return async function(event: any): Promise<void> {
+            let session = await loadSessionAsync(bot, event);
+
+            if (event.membersAdded && event.membersAdded[0].id && event.membersAdded[0].id.endsWith(config.get("bot.botId"))) {
+                session.send(Strings.bot_introduction); // probably only works in Teams
+            } else {
+                session.send(Strings.bot_welcome_to_new_person);
+            }
+        };
+    }
+}
---
+++ microsoft-teams-compose-extensions/src/app.ts
@@ -0,0 +1,109 @@
+import { Request, Response } from "express";
+let express = require("express");
+let favicon = require("serve-favicon");
+let http = require("http");
+let path = require("path");
+let config = require("config");
+import { Bot } from "./Bot";
+import { VSTSTokenOAuth2API } from "./apis/VSTSTokenOAuth2API";
+import * as teams from "botbuilder-teams";
+import { LoadingTab } from "./pages/LoadingTab";
+import { DefaultTab } from "./pages/DefaultTab";
+import { AllCommandsTab } from "./pages/AllCommandsTab";
+import { VSTSAuthTab } from "./pages/VSTSAuthTab";
+import { VSTSAuthFlowStartPopUp } from "./pages/VSTSAuthFlowStartPopUp";
+import { VSTSAuthFlowEndPopUp } from "./pages/VSTSAuthFlowEndPopUp";
+import { ComposeExtensionSettingsPopUp } from "./pages/ComposeExtensionSettingsPopUp";
+import { MongoDbBotStorage } from "./storage/MongoDbBotStorage";
+import { MongoDbBotChannelStorage } from "./storage/MongoDbBotChannelStorage";
+import { AADUserValidation } from "./apis/AADUserValidation";
+
+// Configure instrumentation - tooling with Azure
+// let appInsights = require("applicationinsights");
+// let instrumentationKey = config.get("app.instrumentationKey");
+// if (instrumentationKey) {
+//     appInsights.setup(instrumentationKey).start();
+// }
+
+let app = express();
+
+app.set("port", process.env.PORT || 4040);
+app.use(express.static(path.join(__dirname, "../../public")));
+app.use(express.static(path.join(__dirname, "./public"))); // used for static dialogs
+app.use(favicon(path.join(__dirname, "../../public/assets", "favicon.ico")));
+
+// Tab and Popup urls
+app.get("/loading", LoadingTab.getRequestHandler());
+app.get("/default", DefaultTab.getRequestHandler());
+app.get("/allCommands", AllCommandsTab.getRequestHandler());
+app.get("/vstsAuth", VSTSAuthTab.getRequestHandler());
+app.get("/vstsAuthFlowStart", VSTSAuthFlowStartPopUp.getRequestHandler());
+app.get("/vstsAuthFlowEnd", VSTSAuthFlowEndPopUp.getRequestHandler());
+app.get("/composeExtensionSettings", ComposeExtensionSettingsPopUp.getRequestHandler());
+
+// Create Teams connector for the bot
+let connector = new teams.TeamsChatConnector({
+    appId: config.get("bot.botId"),
+    appPassword: config.get("bot.botPassword"),
+});
+
+// Create storage for the bot
+let channelStorage = null;
+let botStorage = null;
+if (config.get("channelStorageType") === "mongoDb") {
+    channelStorage = new MongoDbBotChannelStorage(config.get("mongoDb.botStateCollection"), config.get("mongoDb.connectionString"));
+    botStorage = new MongoDbBotStorage(config.get("mongoDb.botStateCollection"), config.get("mongoDb.connectionString"));
+};
+
+let botSettings = {
+    channelStorage: channelStorage,
+    storage: botStorage,
+};
+
+let bot = new Bot(connector, botSettings);
+
+// Configure bot routes
+app.post("/api/messages", connector.listen());
+app.get("/api/VSTSOauthCallback", VSTSTokenOAuth2API.setUserAccessToken(bot));
+app.get("/api/validateUser", AADUserValidation.validateUser(bot));
+app.get("/api/success", AADUserValidation.success(bot));
+
+// catch 404 and forward to error handler
+app.use((req: Request, res: Response, next: Function) => {
+    let err: any = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get("env") === "development") {
+    app.use(function(err: any, req: Request, res: Response, next: Function): void {
+        res.status(err.status || 500);
+        res.render("error", {
+            message: err.message,
+            error: err,
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err: any, req: Request, res: Response, next: Function): void {
+    res.status(err.status || 500);
+    res.render("error", {
+        message: err.message,
+        error: {},
+    });
+});
+
+let endpoint = config.get("app.baseUri") || "unknown";
+
+http.createServer(app).listen(app.get("port"), function (): void {
+    console.log("Express server listening on port " + app.get("port"));
+    console.log("Public bot messaging endpoint: " + endpoint + "/api/messages");
+});
+
+module.exports = app;
